5701408bd34b8c43d981995342dbb68afade088b
Pramtoxz <pramuditometra@gmail.com>
2025-12-16 11:37:11 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; {{ date('Y') }}
-                        <a href="." class="link-secondary">Perusahaan Anda</a>.
+                        <a href="." class="link-secondary">Tugas Akhir Mobile 2|Kelompok Rental Mobil</a>.
                     </li>
                 </ul>
             </div>
